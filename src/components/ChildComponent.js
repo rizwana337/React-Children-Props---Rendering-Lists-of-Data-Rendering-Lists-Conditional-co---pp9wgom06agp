a866f60d1ab58cfe16e004c81a4fc2aeaffd0039
SYED RIYAZ <syedriyaz@192.168.1.66>
2023-02-27 16:09:25 +0530

    add

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
 const ChildComponent = () => {
-  const [value, setValue] = useState(0);
-  const onChangeHandler = (e) => {
-    setValue(e.target.value);
-  };
+  
 //code here
   return (
     <div id="child">
