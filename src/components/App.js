22c4ee7a498031285f9b28dcac058fafc7237cbf
SYED RIYAZ <syedriyaz@192.168.1.66>
2023-02-27 13:25:13 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,11 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
- const [value, setValue] = useState(0);
-  const onChangeHandler = (e) => {
-    setValue(e.target.value);
-  };
+ 
   return (
     <div id="main">
       <ChildComponent /> 
-      <input id="input" onChange={onChangeHandler} />
-
-<p id="output">{Math.sqrt(value)}</p>
+      
     </div>
   )
 }
