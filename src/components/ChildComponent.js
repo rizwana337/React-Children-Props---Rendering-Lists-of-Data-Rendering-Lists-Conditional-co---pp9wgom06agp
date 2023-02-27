46d99d3cac607cfd76ab283ec52ac3809d31d9a4
SYED RIYAZ <syedriyaz@192.168.1.66>
2023-02-27 13:04:58 +0530

    add

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const ChildComponent = () => {
+  const [value, setValue] = useState(0);
+  const onChangeHandler = (e) => {
+    setValue(e.target.value);
+  };
 //code here
   return (
     <div id="child">
       Find The Square <br/>
-     
+     <input id="input" onChange={onChangeHandler} />
+
+      <p id="output">{Math.sqrt(value)}</p>
     </div>
   )
 }
