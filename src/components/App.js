9f37b8240e29228c871b5c3c93a800b0feb9ea33
SYED RIYAZ <syedriyaz@192.168.1.6>
2023-03-02 23:45:03 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,17 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
- const [value, setValue] = useState(0);
-  const onChangeHandler = (e) => {
-    setValue(e.target.value);
-  };
+ const [value, setValue] = useState('');
+  const onChangeHandler = (prop) => {
+    setValue(prop.target.value);
+  }
   return (
     <div id="main">
-      <ChildComponent /> 
+      <ChildComponent > 
        
       <input id="input" onChange={onChangeHandler} />
-
-      <p id="output">{Math.sqrt(value)}</p>
+</ChildComponent>
+      <p id="output">{Number(value)*Number(value)}</p>
     </div>
   )
 }
--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const ChildComponent = () => {
-  
 //code here
   return (
     <div id="child">
       Find The Square <br/>
-      
+      {props.children}
     </div>
     
   )
-}
+};
 
 
 export default ChildComponent;
